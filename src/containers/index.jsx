740b743ff357b8c7c868fbd20c314cc041c3094e
zjy <3152811479@qq.com>
2019-01-06 19:05:21 +0800

    项目初始化

---
+++ src/containers/index.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+class App extends React.Component{
+    render(){
+        return <div>app</div>
+    }
+}
+export default App
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react"
+import ReactDom from "react-dom"
+import "common/css/style.css"
+import App from "views"
+ReactDom.render(
+    <App />,
+    document.getElementById("root")
+)
